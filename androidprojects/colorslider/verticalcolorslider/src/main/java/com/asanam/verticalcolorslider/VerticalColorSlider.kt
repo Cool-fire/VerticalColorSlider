d3f85ed22ce3d5a7d9069e0e0fec2ea097dbb409
Cool-fire <upendrareddy2511@gmail.com>
2019-09-26 22:28:41 +0530

    Add lifecycle methods

--- androidprojects/colorslider/verticalcolorslider/src/main/java/com/asanam/verticalcolorslider/VerticalColorSlider.kt
+++ androidprojects/colorslider/verticalcolorslider/src/main/java/com/asanam/verticalcolorslider/VerticalColorSlider.kt
@@ -1,17 +1,27 @@
 package com.asanam.verticalcolorslider
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
 class VerticalColorSlider(context:Context, attrs:AttributeSet) : View(context, attrs) {
+    private lateinit var bitmap: Bitmap
+    private var cacheBitmap: Boolean = true
+    private lateinit var path: Path
+    private val onColorChangeListener: OnColorChangeListener? = null
+    private var selectorYPos: Float = 0F
+    private lateinit var colorPickerBody: RectF
+    private var colorPickerRadius: Float = 0F
+    private var centerX: Int = 0
     private lateinit var paint: Paint
     private lateinit var strokePaint: Paint
     private var borderColor : Int
     private var borderWidth : Float
-    private lateinit var colors : IntArray
+    private var colors : IntArray
+    private var viewWidth : Int = 0
+    private var viewHeight : Int = 0
+
     init {
         context.theme.obtainStyledAttributes(
             attrs,
@@ -52,4 +62,51 @@ class VerticalColorSlider(context:Context, attrs:AttributeSet) : View(context, a
             strokeWidth = borderWidth
         }
     }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        viewWidth = w
+        viewHeight = h
+
+        centerX = viewWidth/2
+        colorPickerRadius = (viewWidth/2) - borderWidth
+        colorPickerBody = RectF(centerX - colorPickerRadius, borderWidth + colorPickerRadius, centerX + colorPickerRadius, viewHeight - (borderWidth + colorPickerRadius))
+
+        paint.shader = LinearGradient(0F, colorPickerBody.top,0F,colorPickerBody.bottom, colors,null,Shader.TileMode.CLAMP)
+
+        reset()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        path.apply {
+            addCircle(centerX.toFloat(), borderWidth + colorPickerRadius, colorPickerRadius, Path.Direction.CW)
+            addRect(colorPickerBody, Path.Direction.CW)
+            addCircle(centerX.toFloat(), viewHeight - (borderWidth + colorPickerRadius), colorPickerRadius, Path.Direction.CW)
+        }
+
+        canvas?.apply {
+            drawPath(path, strokePaint)
+            drawPath(path, paint)
+        }
+        if (cacheBitmap) {
+            bitmap = getDrawingCache()
+            cacheBitmap = false
+            invalidate()
+        } else {
+            canvas?.drawLine(colorPickerBody.left, selectorYPos, colorPickerBody.right, selectorYPos, strokePaint)
+        }
+    }
+    
+    private fun reset() {
+        selectorYPos = borderWidth + colorPickerRadius
+        onColorChangeListener?.onColorChange(Color.TRANSPARENT)
+        invalidate()
+    }
+
+    interface OnColorChangeListener {
+        fun onColorChange(selectedColor: Int)
+    }
+
 }
